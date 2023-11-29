96b6c58f941dbd3392009b61817a1206547d3365
mohiteprathamesh1996 <mohite.p@northeastern.edu>
2023-11-28 21:42:39 -0500

    Completed Step 5: Model Evaluation

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -3,7 +3,8 @@ from sensor.entity.config_entity import (
     DataIngestionConfig,
     DataValidationConfig,
     DataTransformationConfig,
-    ModelTrainerConfig
+    ModelTrainerConfig,
+    ModelEvaluationConfig
     )
 
 
@@ -13,13 +14,15 @@ from sensor.logger import logging
 from sensor.entity.artifact_entity import (
     DataIngestionArtifact,
     DataValidationArtifact,
-    DataTransformationArtifact
+    DataTransformationArtifact,
+    ModelTrainerArtifact
     )
 
 from sensor.component.data_ingestion import DataIngestion
 from sensor.component.data_validation import DataValidation
 from sensor.component.data_transformation import DataTransformation
 from sensor.component.model_trainer import ModelTrainer
+from sensor.component.model_evaluation import ModelEvaluation
 
 import os, sys
 
@@ -85,11 +88,20 @@ class TrainPipeline:
         except  Exception as e:
             raise  SensorException(e,sys)
     
-    def start_model_evaluation(self):
+    def start_model_evaluation(
+            self,
+            data_validation_artifact:DataValidationArtifact,
+            model_trainer_artifact:ModelTrainerArtifact
+            ):
         try:
-            pass
-        except Exception as e:
-            raise SensorException(e, sys)
+            model_eval_config = ModelEvaluationConfig(self.training_pipeline_config)
+            model_eval = ModelEvaluation(model_eval_config, data_validation_artifact, model_trainer_artifact)
+            model_eval_artifact = model_eval.initiate_model_evaluation()
+            
+            return model_eval_artifact
+        
+        except  Exception as e:
+            raise  SensorException(e,sys)
     
     def start_model_pusher(self):
         try:
@@ -103,6 +115,10 @@ class TrainPipeline:
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact = self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_evaluation_artifact = self.start_model_evaluation(
+                data_validation_artifact=data_validation_artifact,
+                model_trainer_artifact=model_trainer_artifact
+                )
             
         except Exception as e:
             raise SensorException(e, sys)

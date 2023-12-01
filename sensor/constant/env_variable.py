a8a7bf6564a03edd1f4fa90f9a01aa3489bbcf1e
mohiteprathamesh1996 <mohite.p@northeastern.edu>
2023-12-01 15:49:38 -0500

    Updates

--- sensor/constant/env_variable.py
+++ sensor/constant/env_variable.py
@@ -5,8 +5,8 @@ import os
 #load_dotenv()
 
 try:
-  MONGODB_URL = os.getenv('MONGODB_URL')
-  MONGO_DB_URL = os.getenv('MONGODB_URL')
+  MONGODB_URL = os.environ.get('MONGO_DB_URL')
+  MONGO_DB_URL = os.environ.get('MONGO_DB_URL')
   AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
   AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
   AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")

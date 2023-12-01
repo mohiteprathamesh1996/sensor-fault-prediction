7d739aca6ae6751889244f2b444d463afce50dbd
mohiteprathamesh1996 <mohite.p@northeastern.edu>
2023-12-01 16:58:39 -0500

    Ultra commit - 2

--- sensor/constant/env_variable.py
+++ sensor/constant/env_variable.py
@@ -1,14 +1,11 @@
 import os
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
 # Load env
-#load_dotenv()
+load_dotenv()
 
-try:
-  MONGODB_URL = os.environ.get('MONGO_DB_URL')
-  MONGO_DB_URL = os.environ.get('MONGO_DB_URL')
-  AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-  AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-  AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")
-except:
-  pass
+MONGODB_URL = os.environ.get('MONGO_DB_URL')
+MONGO_DB_URL = os.environ.get('MONGO_DB_URL')
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+AWS_DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION")

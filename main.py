0dd2bf975e17208db4b020de16b8aa24121b4f89
mohiteprathamesh1996 <mohite.p@northeastern.edu>
2023-12-01 10:18:46 -0500

    Improvised code structure

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
-from sensor.configuration.mongo_db_connection import MongoDBClient
-from sensor.exception import SensorException
-import os,sys
+import os
+import io
 import numpy as np
-from sensor.entity.artifact_entity import DataValidationArtifact
-from sensor.logger import logging
-from sensor.pipeline import training_pipeline
-from sensor.pipeline.training_pipeline import TrainPipeline
 import pandas as pd
-from sensor.utils.main_utils import read_yaml_file
-from sensor.constant.training_pipeline import *
-from sensor.constant import training_pipeline
 from fastapi import FastAPI, UploadFile, File
-from sensor.constant.application import APP_HOST, APP_PORT
 from starlette.responses import RedirectResponse
 from uvicorn import run as app_run
 from fastapi.responses import Response
-from sensor.ml.model.estimator import ModelResolver,TargetValueMapping
-from sensor.utils.main_utils import load_object
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-import io
 
-directory = training_pipeline.ARTIFACT_DIR
-
-
-
+from sensor.logger import logging
+from sensor.pipeline.training_pipeline import TrainPipeline
+from sensor.constant.training_pipeline import (
+    DATA_INGESTION_DIR_NAME,
+    DATA_INGESTION_INGESTED_DIR,
+    TRAIN_FILE_NAME,
+    SAVED_MODEL_DIR,
+    TARGET_COLUMN
+)
+from sensor.constant.training_pipeline import ARTIFACT_DIR
+from sensor.constant.application import APP_HOST, APP_PORT
+from sensor.ml.model.estimator import ModelResolver
+from sensor.utils.main_utils import load_object
 
 app = FastAPI()
 origins = ["*"]
@@ -42,6 +38,7 @@ app.add_middleware(
 async def index():
     return RedirectResponse(url="/docs")
 
+
 @app.get("/train")
 async def train_route():
     try:
@@ -54,51 +51,53 @@ async def train_route():
     except Exception as e:
         return Response(f"Error Occurred! {e}")
 
-class InputData(BaseModel):
-    features: dict
 
 @app.post("/predict")
 async def predict_route(file: UploadFile = File(...)):
     try:
         contents = await file.read()  # Read the contents of the uploaded file
-        df = pd.read_csv(io.BytesIO(contents))  # Use io.BytesIO to read bytes as CSV
+        # Use io.BytesIO to read bytes as CSV
+        df = pd.read_csv(io.BytesIO(contents))
         # Process the DataFrame (e.g., perform operations, save to disk, etc.)
-        directory = training_pipeline.ARTIFACT_DIR
+        directory = ARTIFACT_DIR
         files_with_timestamps = [
             (
-                filename, 
+                filename,
                 os.path.getctime(os.path.join(directory, filename))
-                )
-                for filename in os.listdir(directory)
-                ]
-        
-        latest_file = max(files_with_timestamps, key=lambda x: x[1])[0] if files_with_timestamps else None
+            )
+            for filename in os.listdir(directory)
+        ]
+
+        latest_file = max(files_with_timestamps, key=lambda x: x[1])[
+            0] if files_with_timestamps else None
 
         latest_file_path_csv = os.path.join(
-            directory, 
-            latest_file, 
+            directory,
+            latest_file,
             DATA_INGESTION_DIR_NAME,
-            DATA_INGESTION_INGESTED_DIR, 
+            DATA_INGESTION_INGESTED_DIR,
             TRAIN_FILE_NAME
-            )
-        
+        )
+
         cols_to_include = list(pd.read_csv(latest_file_path_csv).columns)
 
         model_resolver = ModelResolver(model_dir=SAVED_MODEL_DIR)
-        
+
         if not model_resolver.is_model_exists():
             return Response("Model is not available")
-            
+
         best_model_path = model_resolver.get_best_model_path()
         model = load_object(file_path=best_model_path)
         y_pred = model.predict(
-            x = df[cols_to_include].drop(columns=TARGET_COLUMN).replace('na', np.nan)
-            )
-        
+            x=df[cols_to_include].drop(
+                columns=TARGET_COLUMN
+            ).replace('na', np.nan)
+        )
+
     except Exception as e:
         return Response(f"Error Occurred! {e}")
-    
-    return {"predictions" : y_pred.tolist()}
+
+    return {"predictions": y_pred.tolist()}
 
 
 def main():
@@ -110,7 +109,6 @@ def main():
         logging.exception(e)
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     main()
     app_run(app, host=APP_HOST, port=APP_PORT)
-    
